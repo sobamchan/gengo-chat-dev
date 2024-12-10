d19bc46dc40ca94085e310cdea3a76783964a70c
sobamchan <oh.sore.sore.soutarou@gmail.com>
2024-12-10 15:35:41 +0100

    text white h3

--- src/lib/components/UserPost.svelte
+++ src/lib/components/UserPost.svelte
@@ -1,25 +1,22 @@
 <script>
-    import { marked } from 'marked';
-    import Icon from '@iconify/svelte';
+	import { marked } from 'marked';
+	import Icon from '@iconify/svelte';
 
-    export let msg;
+	export let msg;
 </script>
 
-
 <div class="border-solid border-2 rounded-md">
-    <div class="m-4">
-        <div>
-            <span class="badge variant-filled-surface mb-2 text-sm">
-                You
-            </span>
+	<div class="m-4">
+		<div>
+			<span class="badge variant-filled-surface mb-2 text-sm"> You </span>
 
-            <button class="float-right">
-                <Icon icon="mynaui:copy" class="inline"/>
-            </button>
-        </div>
+			<button class="float-right">
+				<Icon icon="mynaui:copy" class="inline" />
+			</button>
+		</div>
 
-        <div class=
-            "
+		<div
+			class="
             p-4
             prose
             prose-sm
@@ -29,10 +26,15 @@
             prose-p:w-full
             prose-strong:text-white
             prose-strong:font-semibold
+            prose-h1:text-white
+            prose-h2:text-white
+            prose-h3:text-white
+            prose-h4:text-white
             prose-li:text-white
+            prose-a:text-white
             "
-        >
-            {@html marked.parse(msg.content)}
-        </div>
-    </div>
+		>
+			{@html marked.parse(msg.content)}
+		</div>
+	</div>
 </div>
